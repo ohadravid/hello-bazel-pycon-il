4b0bf6d9f01672c31800f25e5cd3b5e8530496b8
Ohad Ravid <ohad.rv@gmail.com>
2025-08-30 13:03:11 +0300

    Avoid incremental loading because it doesn't work with newer Docker versions

--- integration_test.py
+++ integration_test.py
@@ -63,58 +63,13 @@ def OCIImageContainer(image):
         config = json.load(configp)
 
     client = docker.from_env()
-
-    # Happy path: check if the image exists in the cache
-    try:
-        probe = tar(
-            manifest_json(
-                layers=[layer["digest"] + ".tar" for layer in manifest["layers"]]
-            ),
-            (
-                "config.json",
-                manifest["config"]["size"],
-                open_blob(image, manifest["config"]["digest"]),
-            ),
-        )
-        client.images.load(probe.getvalue())
-        logger.debug("Image was a cache hit")
-        return DockerContainer(manifest["config"]["digest"])
-    except docker.errors.ImageLoadError as e:
-        logger.debug("Image was a cache miss because %s" % e)
-
-    # Unhappy path: image is not in the cache, so we need to load it incrementally
+    
     layers = manifest["layers"]
-    start_from = 0
-
-    # Probe the cache for the first layer that is not in the cache
-    for i in range(1, len(layers)):
-        previous_layer = layers[i - 1]
-        layer = layers[i]
-
-        probe = tar(
-            manifest_json(
-                layers=[layer["digest"] + ".tar" for layer in manifest["layers"][:i]]
-            ),
-            config_json(diff_ids=config["rootfs"]["diff_ids"][:i]),
-        )
-
-        try:
-            probe_res = client.images.load(probe.getvalue())
-            logger.debug("Layer %s was a cache hit" % layer["digest"], probe_res[0].id)
-        except docker.errors.ImageLoadError as e:
-            logger.debug("Layer %s was a cache miss because %s" % (layer["digest"], e))
-            # From this point on we need to load everything.
-            start_from = i - 1
-            break
-
-    logger.debug("Need to load all the layers after: ", start_from)
-
     # Load all layers at once
     load = tar(
-        # Send only needed layers
         *[
             (layer["digest"] + ".tar", layer["size"], open_blob(image, layer["digest"]))
-            for layer in layers[start_from:]
+            for layer in layers
         ],
         manifest_json(
             layers=[layer["digest"] + ".tar" for layer in manifest["layers"]]
@@ -128,15 +83,7 @@ def OCIImageContainer(image):
 
     load_res = client.images.load(load.getvalue())
 
-    # Extra safety check: assert that the final image id is equal to the one in the manifest
-    assert (
-        load_res[0].id == manifest["config"]["digest"]
-    ), "final image id should match manifest %s == %s" % (
-        load_res[0].id,
-        manifest["config"]["digest"],
-    )
-
-    return DockerContainer(manifest["config"]["digest"])
+    return DockerContainer(load_res[0].id)
 
 
 import requests
